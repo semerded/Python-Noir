9ac597e7336593d8820ba41b68b49947261b279e
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-04-02 11:53:32 +0200

    create skeleton for mystery page

--- mystery/mystery.js
+++ mystery/mystery.js
@@ -1,1 +1,38 @@
-console.log(API_KEY);
+// you have to request access to the proxy server with the following link:
+// https://cors-anywhere.herokuapp.com/corsdemo
+
+const apiUrl = 'https://glot.io/api/run/python/latest';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Proxy URL
+const url = proxyUrl + apiUrl;
+
+
+function submitCode() {
+    const code = document.getElementById("code-area").value;
+    console.log(code);
+
+    const data = {
+        files: [{
+          name: 'main.py',
+          content: code
+        }]
+      };
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Token ${API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+  .then(data => {
+    console.log('Response:', data);
+    if (data.stdout) {
+      console.log('Output:', data.stdout);
+    }
+    if (data.stderr) {
+      console.error('Error:', data.stderr);
+    }
+  })
+  .catch(error => console.error('Error:', error));
+    }
